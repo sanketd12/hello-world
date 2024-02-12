9fd170b2fab0e2f8e57067bc8a307552b1eea3d5
Sanket Doddabendigere <sanketdoddabendigere@gmail.com>
2024-02-11 22:20:01 -0500

    Add files via upload

---
+++ HW1DS2002.sql
@@ -0,0 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Feb 12, 2024 at 03:18 AM
+-- Server version: 8.0.36-0ubuntu0.22.04.1
+-- PHP Version: 8.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sakila`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `l`
+--
+-- Error reading structure for table sakila.l: #1146 - Table &#039;sakila.l&#039; doesn&#039;t exist
+-- Error reading data for table sakila.l: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near &#039;FROM `sakila`.`l`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
